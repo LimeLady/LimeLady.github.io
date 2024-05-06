038faccf7f33b65b5cfde103427b521b50081da7
emertensitba <emertens@itba.edu.ar>
2024-05-06 12:52:41 -0300

    First final changes
    Added and changed a lot to bring out a MVP

--- js/scripts.js
+++ js/scripts.js
@@ -28,5 +28,36 @@ document.addEventListener("DOMContentLoaded", function() {
 document.querySelectorAll('.project-image').forEach(function(image) {
     image.addEventListener('click', function() {
         this.classList.toggle('zoomed');
+        document.querySelectorAll('.arrow').forEach(function(arrow) {
+            arrow.classList.toggle('hidden');
+        });
     });
+});
+
+var images = [
+    
+    'images/projects/Dashboard screenshot 2.jpg',
+    'images/projects/Dashboard screenshot.jpg',
+    'images/projects/Dashboard screenshot 3.png',
+    'images/projects/Dashboard screenshot 4.png',
+    'images/projects/Dashboard screenshot 5.png',
+    // add more image paths as needed
+];
+
+var currentImageIndex = 0;
+
+document.querySelector('.left-arrow').addEventListener('click', function() {
+    currentImageIndex--;
+    if (currentImageIndex < 0) {
+        currentImageIndex = images.length - 1;
+    }
+    document.querySelector('.project-image').src = images[currentImageIndex];
+});
+
+document.querySelector('.right-arrow').addEventListener('click', function() {
+    currentImageIndex++;
+    if (currentImageIndex >= images.length) {
+        currentImageIndex = 0;
+    }
+    document.querySelector('.project-image').src = images[currentImageIndex];
 });
